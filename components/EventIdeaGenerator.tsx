70ad706b3241adda38ac60946b22423ee3f6d2b5
outofcircles <outofcircles2025@gmail.com>
2025-11-01 15:17:07 +0530

    Delete components directory

--- components/EventIdeaGenerator.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const EventIdeaGenerator: React.FC = () => {
-  // This component is disabled to remove the API key requirement.
-  return null;
-};
-
-export default EventIdeaGenerator;
